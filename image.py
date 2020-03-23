78f156c1014da9fa3e4750b872ef8730736b9b64
Ian Fried <ifried00@stuy.edu>
2020-03-22 23:02:53 -0400

    final update...wasn't good about using github this time around lol

--- image.py
+++ image.py
@@ -1,29 +1,27 @@
 #SPECIAL THANKS TO IAN WILLIAMS FOR HELPING ME WITH SOME DEBUGGING, PRAISE PROGRAMMER JESUS, PEACE BE UPON HIM
 #<3 IW
-import math
+from math import cos, sin, radians, pi as PI
 import os
-#multiply m1 by m2, modifying m2 to be the product
-#m1 * m2 -> m2
-def matrix_mult( m1, m2 ): #IW Fixed your matrix multiplication function to work with non 4xn matrices
+
+# Core Matrix functionality
+def matrix_mult( m1, m2 ):
+    """ Multiply matrix m2 by m1, changing m2. """
     temp = new_matrix(len(m1), len(m2[0]))
     for row in range(len(temp)):
         for col in range(len(temp[0])):
             for i in range(len(m1[0])):
                 temp[row][col] += m1[row][i] * m2[i][col]
-    m2 = temp
-    return temp
-
-    point = 0
-    for row in m2:
-        #get a copy of the next point
-        tmp = row[:]
-
-        for r in range(4):
-            m2[point][r] = (m1[0][r] * tmp[0] +
-                            m1[1][r] * tmp[1] +
-                            m1[2][r] * tmp[2] +
-                            m1[3][r] * tmp[3])
-        point+= 1
+    i = 0
+    while i < len(m2):
+        if m2[i]:
+            m2[i] = temp[i]
+        else:
+            m2.append(temp[i])
+        i += 1
+    return m2
+def display_matrix(x):
+    for i in x:
+        print(i)
 
 def ident( matrix ):
     for r in range( len( matrix[0] ) ):
@@ -47,6 +45,8 @@ def new_matrix(rows = 4, cols = 4):
             m[c].append( 0 )
     return m
 
+
+# Misc. useful matrixes to have
 def make_hermite():
     t = [[2, -2, 1, 1], [-3, 3, -2, -1], [0, 0, 1, 0], [1, 0, 0, 0]]
     return t
@@ -54,9 +54,9 @@ def make_hermite():
 def make_translate( x, y, z ):
     t = new_matrix()
     ident(t)
-    t[3][0] = x
-    t[3][1] = y
-    t[3][2] = z
+    t[0][3] = x
+    t[1][3] = y
+    t[2][3] = z
     return t
 
 def make_scale( x, y, z ):
@@ -70,36 +70,72 @@ def make_scale( x, y, z ):
 def make_rotX( theta ):
     t = new_matrix()
     ident(t)
-    t[1][1] = math.cos(theta)
-    t[2][1] = -1 * math.sin(theta)
-    t[1][2] = math.sin(theta)
-    t[2][2] = math.cos(theta)
+    t[1][1] = cos(theta)
+    t[1][2] = - sin(theta)
+    t[2][1] = sin(theta)
+    t[2][2] = cos(theta)
     return t
 
 def make_rotY( theta ):
     t = new_matrix()
     ident(t)
-    t[0][0] = math.cos(theta)
-    t[0][2] = -1 * math.sin(theta)
-    t[2][0] = math.sin(theta)
-    t[2][2] = math.cos(theta)
+    t[0][0] = cos(theta)
+    t[0][2] = sin(theta)
+    t[2][0] = - sin(theta)
+    t[2][2] = cos(theta)
     return t
 
 def make_rotZ( theta ):
     t = new_matrix()
     ident(t)
-    t[0][0] = math.cos(theta)
-    t[1][0] = -1 * math.sin(theta)
-    t[0][1] = math.sin(theta)
-    t[1][1] = math.cos(theta)
+    t[0][0] = cos(theta)
+    t[0][1] = - sin(theta)
+    t[1][0] = sin(theta)
+    t[1][1] = cos(theta)
     return t
 
+
 def circle_point(x, y, theta, r):
-    xn = x + (r * math.cos(math.radians(theta)))
-    yn = y + (r * math.sin(math.radians(theta)))
+    """ Returns a point in a polar way. Centered at point (x, y), returns the point r away at an angle of theta degrees. """
+    xn = x + (r * cos(radians(theta)))
+    yn = y + (r * sin(radians(theta)))
     return [xn, yn]
 
-class picture:
+
+# Point generation for useful 3d shapes. And toruses...
+def generate_sphere(x, y, z, r):
+    """ Generates the points on a sphere, stores them in a 3xn array, and returns them. """
+    m = new_matrix(3, 1)
+    t_inc = PI /  30
+    theta = 0
+    while theta < 2 * PI:
+        phi = 0
+        while phi < PI:
+            m[0].append(x + (r * cos(theta) * sin(phi)))
+            m[1].append(y + (r * sin(theta) * sin(phi)))
+            m[2].append(z + (r * cos(phi)))
+            phi += t_inc
+            theta += t_inc
+    return m
+
+def generate_torus(x, y, z, r, R):
+    """ Generates the points on a torus, stores them in a 3xn array, and returns them. """
+    m = new_matrix(3, 1)
+    t_inc = PI /  30
+    theta = 0
+    while theta < 2 * PI:
+        phi = 0
+        while phi < 2 * PI:
+            m[0].append(x + (cos(theta) * (r * cos(phi) + R)))
+            m[1].append(y + (r * sin(phi)))
+            m[2].append(z - (sin(theta) * (r * cos(phi) + R)))
+            phi += t_inc
+            theta += t_inc
+    return m
+
+
+class Picture:
+    """ A class that represents a canvas, upon which artists can create beautiful works."""
     def __init__(self, n, w, h):
         self.name = n
         self.width, self.height = (w, h)
@@ -109,11 +145,13 @@ class picture:
         self.transformation_matrix = ident(new_matrix())
 
     def plot(self, x, y, color):
+        """ Plot a point on the screen. """
         if x < 0 or y < 0 or x >= self.width or y >= self.height:#out of bounds
             return
         self.pixels[int(self.height - y - 1)][int(x)] = color #Flip for humans
 
     def draw_line(self, x0, y0, x1, y1, color ):
+        """ Draw a line on the screen. """
         #swap points if going right -> left
         if x0 > x1:
             xt = x0
@@ -197,21 +235,20 @@ class picture:
     def display_edge_matrix(self):
         for i in self.edge_matrix:
             print(i)
-
     def add_3d_point(self, x, y, z):
+        """ Add a single point to the edge matrix. """
         self.edge_matrix[0].append(x)
         self.edge_matrix[1].append(y)
         self.edge_matrix[2].append(z)
         self.edge_matrix[3].append(1)
-        #print x, y, z
 
     def add_edge(self, x0, y0, z0, x1, y1, z1):
+        """ Add an edge to the edge matrix. """
         self.add_3d_point(x0,y0,z0)
         self.add_3d_point(x1,y1,z1)
 
-
     def add_circle(self, cx, cy, cz, r, step ):
-
+        """ Add a circle to the edge matrix. """
         centx = cx
         centy = cy
         theta = 0
@@ -225,61 +262,61 @@ class picture:
             last_pos = secondpoints
             theta += step
 
-
     def add_curve(self, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type ):
+        """ Add a curve, either hermite or bezier, to the edge matrix. """
         t = 0
         if curve_type == "bezier":
-            # I can't remember how this works, so i reimplemented it IW
-            """
-            xa = (-1 * x0) + (3 * x1) - (3 * x2) + (x3 * 3)
-            xb = (3 * x0) - (6 * x1) + (3 * x2)
-            xc = (-3 * x0) + (3 * x1)
-            xd = x0
-            """
-            #for(t = 0, t < 1, t += step): IW incorrect syntax
-
             while t <= 1:
                 d = [pow(1 - t, 3), 3 * t * pow(1 - t, 2), 3 * pow(t, 2) * (1 - t), pow(t, 3)]
                 x = (x0 * d[0]) + (x1 * d[1]) + (x2 * d[2]) + (x3 * d[3])
                 y = (y0 * d[0]) + (y1 * d[1]) + (y2 * d[2]) + (y3 * d[3])
                 self.add_3d_point(x, y, 0)
-                #plotx = xa * t * t * t + xb * t * t + xc * t + xd
-	        #ploty = ya * t * t * t + yb * t * t + yc * t + yd
-
-	        #dt = t + step
-
-	        #newplotx = xa * dt * dt * dt + xb * dt * dt + xc * dt + xd
-	        #newploty = ya * dt * dt * dt + yb * dt * dt + yc * dt + yd
-
-	        #self.add_edge(plotx, ploty, 1, newplotx, newploty, 1)
                 t += step
         else:
             m = make_hermite()
             pmatrix = [[x0, y0, 1], [x1, y1, 1], [x2, y2, 1], [x3, y3, 1]]
-            #for(t = 0, t < 1, t += step):
             while t <= 1:
-
                 tmatrix = [[t * t * t, t * t, t, 1]]
-                #print "bef", m
-                #matrix_mult(tmatrix, m)
-                #print "aft", m
                 a = matrix_mult(matrix_mult(tmatrix, m), pmatrix)
-                #print a
-
-                #self.add_3d_point(matrix_mult(matrix_mult(tmatrix, m), pmatrix))
                 self.add_3d_point(a[0][0], a[0][1], a[0][2])
-
                 t += step
 
+    def add_box(self, x, y, z, width, height, depth):
+        """ Add a box to the edge matrix. Rectangular Prism is such an ugly word, using box means instead means I take up less space. """
+        # Front face
+        self.add_edge(x, y, z, x + width, y, z)
+        self.add_edge(x, y, z, x, y - height, z)
+        self.add_edge(x + width, y - height, z, x + width, y, z)
+        self.add_edge(x + width, y - height, z, x, y - height, z)
+        # Connect the front and back faces
+        self.add_edge(x, y, z, x, y, z - depth)
+        self.add_edge(x + width, y, z, x + width, y, z - depth)
+        self.add_edge(x, y - height, z, x, y - height, z - depth)
+        self.add_edge(x + width, y - height, z, x + width, y - height, z - depth)
+        # Back face
+        self.add_edge(x, y, z - depth, x + width, y, z - depth)
+        self.add_edge(x, y, z - depth, x, y - height, z - depth)
+        self.add_edge(x + width, y - height, z - depth, x + width, y, z - depth)
+        self.add_edge(x + width, y - height, z - depth, x, y - height, z - depth)
 
-    def draw_lines(self):#,  matrix, screen, color ):
-        #self.display_edge_matrix()
+    def add_sphere(self, x, y, z, r):
+        """ Add a sphere to the edge matrix. """
+        m = generate_sphere(x, y, z, r)
+        for i in range(len(m[0])):
+            self.add_edge(m[0][i], m[1][i], m[2][i],
+                          m[0][i] + 1, m[1][i] + 1, m[2][i] + 1)
+    def add_torus(self, x, y, z, r, R):
+        """ Add a torus to the edge matrix. R is the radius of the torus, r is the radius of the circle. """
+        m = generate_torus(x, y, z, r, R)
+        for i in range(len(m[0])):
+            self.add_edge(m[0][i], m[1][i], m[2][i],
+                          m[0][i] + 1, m[1][i] + 1, m[2][i] + 1)
+    def draw_lines(self):
+        """ Draw the edges in the edge matrix. """
         matrix = self.edge_matrix
-        #display_matrix(matrix)
         if len(matrix) < 2:
             print('Need at least 2 points to draw')
             return
-
         point = 0
         while point < len(matrix[0]) - 1:
             self.draw_line( int(matrix[0][point]),
@@ -289,66 +326,49 @@ class picture:
                        [255, 255, 255])
             point+= 2
 
-def print_pic_ascii(g):
-    s = ""
-    for x in range(g.width):
-        for y in range(g.height):
-            n = g.pixels[x][y]
-            s += str(n[0]) + " " + str(n[1]) + " " + str(n[2]) + "  "
-        s += "\n"
-    #print(s)
-    return s
+    def clear_screen(self):
+        """ Clear the pixels in the screen. """
+        self.pixels = [[[0, 0, 0] for i in range(self.width)] for j in range(self.height)]
+    def clear_edge_matrix(self):
+        """ Clear the edge matrix. Useful for removing shapes you no longer want anything to do with. """
+        self.edge_matrix = [[], [], [], []]
 
-def ppm_save_ascii(g):
-    n = str(g.name)
-    f = open(n + ".ppm", "w")
-    f.write("P3\n" + str(g.width) + " " + str(g.height) + "\n255\n")
-    f.write(print_pic_ascii(g))
-    f.close()
-    print(n + '.ppm')
+    def pixels_to_ascii(self):
+        """ Returns the picture in ppm string format. """
+        s = ""
+        for x in range(self.width):
+            for y in range(self.height):
+                n = self.pixels[x][y]
+                s += str(n[0]) + " " + str(n[1]) + " " + str(n[2]) + "  "
+                s += "\n"
+        return s
 
-# def test(x, y, deltx, delty, y_int):
-#     return (delty *  x) - (deltx *  y) + (deltx * y_int)
+    def save(self):
+        """ Writes the picture to a file. """
+        n = str(self.name)
+        f = open(n + ".ppm", "w")
+        f.write("P3\n" + str(self.width) + " " + str(self.height) + "\n255\n")
+        f.write(self.pixels_to_ascii())
+        f.close()
+        print(n + '.ppm')
 
-def add_line(g, startx, starty, endx, endy):
-    g.pixels[startx][starty] = [0, 0, 0]
-    g.pixels[endx][endy] = [0, 0, 0]
-    if endx - startx == 0:
-        drawline_straight(g, startx, starty, endy)
-    else:
-        slope = (endy - starty)/(endx - startx)
-        if (0 <= abs(slope) and abs(slope) < 1):
-            drawline_octant_1(g, startx, starty, endx, endy)
-        elif (1 <= abs(slope)):
-            drawline_octant_2(g, startx, starty, endx, endy)
-
-#NEW MATRIX CODE BELOW
-# example_matrix = [[25, 25, 0, 1], [50, 50, 0, 1], [25, 50, 0, 1], [50, 25, 0, 1]]
-
-def display_matrix(x):
-    for i in x:
-        print(i)
-
-ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'bezier', 'hermite', 'circle' ]
+# Commands in a script file for which arguments must be given
+ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'bezier', 'hermite', 'circle', 'box', 'sphere', 'torus' ]
 
 def parse_file( fname, transform, screen, color ):
-
+    """ Parse the script and carry out the appropriate commands. """
     f = open(fname)
     lines = f.readlines()
-
     c = 0
     while c < len(lines):
         line = lines[c].strip()
-        #print ':' + line + ':'
 
         if line in ARG_COMMANDS:
             c+= 1
             args = lines[c].strip().split(' ')
 
         if line == 'line':
-            #print 'LINE\t' + str(args)
-
-            screen.add_edge( edges,
+            screen.add_edge(
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]) )
 
@@ -365,20 +385,27 @@ def parse_file( fname, transform, screen, color ):
                       float(args[3]), float(args[4]), float(args[5]),
                       float(args[6]), float(args[7]), 0.005, "hermite")
 
+        elif line == 'box':
+            screen.add_box(
+                float(args[0]), float(args[1]), float(args[2]),
+                float(args[3]), float(args[4]), float(args[5]) )
+
+        elif line == 'sphere':
+            screen.add_sphere(float(args[0]), float(args[1]), float(args[2]), float(args[3]))
+
+        elif line == 'torus':
+            screen.add_torus(float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]))
+
         elif line == 'scale':
-            #print 'SCALE\t' + str(args)
             t = make_scale(float(args[0]), float(args[1]), float(args[2]))
             matrix_mult(t, transform)
 
         elif line == 'move':
-            #print 'MOVE\t' + str(args)
             t = make_translate(float(args[0]), float(args[1]), float(args[2]))
             matrix_mult(t, transform)
 
         elif line == 'rotate':
-            #print 'ROTATE\t' + str(args)
-            theta = float(args[1]) * (math.pi / 180)
-
+            theta = float(args[1]) * (PI / 180)
             if args[0] == 'x':
                 t = make_rotX(theta)
             elif args[0] == 'y':
@@ -391,35 +418,26 @@ def parse_file( fname, transform, screen, color ):
             ident(transform)
 
         elif line == 'apply':
-            matrix_mult( transform, edges )
+            matrix_mult(transform, screen.edge_matrix )
 
+        elif line == 'clear':
+            screen.edge_matrix = [[], [], [], []]
         elif line == 'display' or line == 'save':
-            #clear_screen(screen)
-            screen.draw_lines()#edges, screen, color)
-            #print print_pic_ascii(screen)
-            ppm_save_ascii(screen)
+            screen.clear_screen()
+            screen.draw_lines()
+            screen.save()
             if line == 'display':
-                #save_extension(screen, args[0])
-                #display(screen)
                 os.system('display *.ppm')
             else:
                 pass
-                #save_extension(screen, args[0])
 
         c+= 1
 
-#TEST CODE BELOW
+def main():
+    screen = Picture('image', 500, 500)
+    transform = [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    color = [255, 255, 255]
+    parse_file( 'script', transform, screen, color )
 
-#draw_line(self, x0, y0, x1, y1, color )
-#add_edge(self, x0, y0, z0, x1, y1, z1)
-#draw_edges(self, color)
-screen = picture('image', 500, 500)
-transform = [ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-color = [255, 255, 255]
-parse_file( 'script', transform, screen, color )
-
-#n = picture('image', 500, 500)
-
-#n.draw_edges([255, 255, 255])
-
-#ppm_save_ascii(n)
+if __name__ == "__main__":
+    main()
